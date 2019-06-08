3c707e62b6ff7f2b27ec200389c1b0b3c777f12b
yocuervoc <yocuervoc@unal.edu.co>
2019-06-08 17:45:22 -0500

    Update CourseService.java

--- src/main/java/sa/course/service/CourseService.java
+++ src/main/java/sa/course/service/CourseService.java
@@ -23,25 +23,32 @@ public class CourseService {
         return entityManager.find(Course.class, code);
     }
 
-    public Course createCourse(Course course) {
+    public void createCourse(Course course) {
         entityManager.persist(course);
-        entityManager.flush();
-        return course;
     }
 
     public Course updateCourse(long code, Course course) {
         Course courseToUpdate = entityManager.find(Course.class, code);
+
         courseToUpdate.setName(course.getName());
         courseToUpdate.setLastName(course.getLastName());
         courseToUpdate.setCabin(course.getCabin());
         courseToUpdate.setCreditCard(course.getCreditCard());
-        entityManager.merge(courseToUpdate);
-        return entityManager.find(Course.class, code);
+        //
+        courseToUpdate.setUserName(course.getUserName());
+        courseToUpdate.setPassword(course.getPassword());
+        courseToUpdate.setPhoneNumber(course.getPhoneNumber());
+        courseToUpdate.setAddress(course.getAddress());
+        courseToUpdate.setCity(course.getCity());
+        courseToUpdate.setAge(course.getAge());
+        courseToUpdate.setAvatar(course.getAvatar());
+        courseToUpdate.setStoreId(course.getStoreId());
+        courseToUpdate.setType(course.getType());
+        return entityManager.merge(courseToUpdate);
     }
 
-    public Course deleteCourse(long code) {
+    public void deleteCourse(long code) {
         Course course = entityManager.find(Course.class, code);
         entityManager.remove(course);
-        return course;
     }
 }
