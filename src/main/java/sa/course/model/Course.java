0fed8309276a9b561696dc9f7241a8cc8fb7217c
yocuervoc <yocuervoc@unal.edu.co>
2019-06-08 17:44:34 -0500

    Update Course.java

--- src/main/java/sa/course/model/Course.java
+++ src/main/java/sa/course/model/Course.java
@@ -17,7 +17,16 @@ public class Course {
     private String lastName;
     private String cabin;
     private String creditCard;
-
+    //
+    private String userName;
+    private String password;
+    private String phoneNumber;
+    private String address;
+    private String city;
+    private int age;
+    private String avatar;
+    private String storeId;
+    private String type;
 
     public Long getCode() {
         return code;
@@ -52,5 +61,68 @@ public class Course {
     }
     public void setCreditCard(String creditCard) {
         this.creditCard = creditCard;
+    }
+    //
+    public String getUserName(){
+      return userName;
+    }
+    public void setUserName(String userName){
+      this.userName = userName;
+    }
+
+    public String getPassword(){
+      return password;
+    }
+    public void setPassword(String password){
+      this.password = password;
+    }
+
+    public String getPhoneNumber(){
+      return phoneNumber;
+    }
+    public void setPhoneNumber(String phoneNumber){
+      this.phoneNumber = phoneNumber;
+    }
+
+    public String getAddress(){
+      return address;
+    }
+    public void setAddress(String address){
+      this.address = address;
+    }
+
+    public String getCity(){
+      return city;
+    }
+    public void setCity(String city){
+      this.city = city;
+    }
+
+    public int getAge(){
+      return age;
+    }
+    public void setAge(int age){
+      this.age = age;
+    }
+    
+    public String getAvatar(){
+      return avatar;
+    }
+    public void setAvatar(String avatar){
+      this.avatar = avatar;
+    }
+
+    public String getStoreId(){
+      return storeId;
+    }
+    public void setStoreId(String storeId){
+      this.storeId = storeId;
+    }
+
+    public String getType(){
+      return type;
+    }
+    public void setType(String type){
+      this.type = type;
     }
 }
